5450f6ab1b08440cf4cef74df72b9e9fd2a29d22
Jeff Nelson <jeff@cinchapi.com>
2017-02-14 10:55:00 -0500

    initial commit

---
+++ src/main/java/com/cinchapi/concourse/lang/Analyzer.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2013-2017 Cinchapi Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cinchapi.concourse.lang;
+
+import java.util.Collections;
+import java.util.Queue;
+import java.util.Set;
+
+import com.cinchapi.concourse.lang.Parser;
+import com.cinchapi.concourse.lang.PostfixNotationSymbol;
+import com.google.common.collect.Sets;
+
+/**
+ * Analyzes a CCL statement and provides summary metrics.
+ * 
+ * @author Jeff Nelson
+ */
+public class Analyzer {
+
+    /**
+     * The provided CCL statement.
+     */
+    private final String ccl;
+
+    /**
+     * The keys that are used within the CCL statement.
+     */
+    private Set<String> keys = null;
+
+    /**
+     * Construct a new instance.
+     * 
+     * @param ccl
+     */
+    public Analyzer(String ccl) {
+        this.ccl = ccl;
+    }
+
+    /**
+     * Return the keys that are used within the CCL statement.
+     * 
+     * @return the keys.
+     */
+    public Set<String> keys() {
+        if(keys == null) {
+            keys = Sets.newHashSet();
+            Queue<PostfixNotationSymbol> symbols = Parser
+                    .toPostfixNotation(ccl);
+            symbols.forEach((symbol) -> {
+                if(symbol instanceof Expression) {
+                    keys.add(((Expression) symbol).getKeyRaw());
+                }
+            });
+            keys = Collections.unmodifiableSet(keys);
+        }
+        return keys;
+    }
+
+}
