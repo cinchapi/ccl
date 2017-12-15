00a330b962f759596fb75a35adc7353e18e0c731
javierlores <javier@cinchapi.com>
2017-12-15 14:31:40 -0500

    Updated dump visitor

--- src/main/java/com/cinchapi/ccl/generator/GrammarDumpVisitor.java
+++ src/main/java/com/cinchapi/ccl/generator/GrammarDumpVisitor.java
@@ -1,102 +1,102 @@
-/* Copyright (c) 2006, Sun Microsystems, Inc.
- * All rights reserved.
+/*
+ * Copyright (c) 2013-2017 Cinchapi Inc.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     * Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Sun Microsystems, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+package com.cinchapi.ccl.generator;
 
 /**
- *  This is an example of how the Visitor pattern might be used to
- *  implement the dumping code that comes with com.cinchapi.ccl.generator.generated.SimpleNode.  It's a bit
- *  long-winded, but it does illustrate a couple of the main points.
- *  <ol>
- *  <li> the visitor can maintain state between the nodes that it visits
- *  (for example the current indentation level).
- *  </li>
- *
- *  <li>if you don't implement a jjtAccept() method for a subclass of
- *  com.cinchapi.ccl.generator.generated.SimpleNode, then com.cinchapi.ccl.generator.generated.SimpleNode's acceptor will get called.
- *  </li>
- *  <li> the utility method childrenAccept() can be useful when
- *  implementing preorder or postorder tree walks.
- *  </li>
- *  </ol>
- *
+ * A visitor pattern implementation of {@link GrammarVisitor} that
+ * prints the node information to std out
  */
-
 public class GrammarDumpVisitor implements GrammarVisitor
 {
-  private int indent = 0;
+    /**
+     * The indent level
+     */
+    private int indent = 0;
 
-  private String indentString() {
-    StringBuffer sb = new StringBuffer();
-    for (int i = 0; i < indent; ++i) {
-      sb.append(' ');
+    /**
+     * Visitor for a {@link SimpleNode}
+     *
+     * @param node the node
+     * @param data the data
+     * @return the data
+     */
+    public Object visit(SimpleNode node, Object data) {
+        System.out.println(indentString() + node +
+                       ": acceptor not unimplemented in subclass?");
+        ++indent;
+        data = node.childrenAccept(this, data);
+        --indent;
+        return data;
     }
-    return sb.toString();
-  }
 
-  public Object visit(SimpleNode node, Object data) {
-    System.out.println(indentString() + node +
-                   ": acceptor not unimplemented in subclass?");
-    ++indent;
-    data = node.childrenAccept(this, data);
-    --indent;
-    return data;
-  }
+    /**
+     * Visitor for a {@link ASTStart}
+     *
+     * @param node the node
+     * @param data the data
+     * @return the data
+     */
+    public Object visit(ASTStart node, Object data) {
+        System.out.println(indentString() + node);
+        ++indent;
+        data = node.childrenAccept(this, data);
+        --indent;
+        return data;
+    }
 
-  public Object visit(ASTStart node, Object data) {
-    System.out.println(indentString() + node);
-    ++indent;
-    data = node.childrenAccept(this, data);
-    --indent;
-    return data;
-  }
+    /**
+     * Visitor for a {@link ASTConj}
+     *
+     * @param node the node
+     * @param data the data
+     * @return the data
+     */
+    public Object visit(ASTConj node, Object data) {
+        System.out.println(indentString() + node);
+        ++indent;
+        data = node.childrenAccept(this, data);
+        --indent;
+        return data;
+    }
 
-  public Object visit(ASTAnd node, Object data) {
-    System.out.println(indentString() + node);
-    ++indent;
-    data = node.childrenAccept(this, data);
-    --indent;
-    return data;
-  }
+    /**
+     * Visitor for a {@link ASTRelationalExpression}
+     *
+     * @param node the node
+     * @param data the data
+     * @return the data
+     */
+    public Object visit(ASTRelationalExpression node, Object data) {
+        System.out.println(indentString() + node);
+        ++indent;
+        data = node.childrenAccept(this, data);
+        --indent;
+        return data;
+    }
 
-  public Object visit(ASTOr node, Object data) {
-    System.out.println(indentString() + node);
-    ++indent;
-    data = node.childrenAccept(this, data);
-    --indent;
-    return data;
-  }
-
-  public Object visit(ASTRelationalExpression node, Object data) {
-    System.out.println(indentString() + node);
-    ++indent;
-    data = node.childrenAccept(this, data);
-    --indent;
-    return data;
-  }
+    /**
+     * Creates an empty string with the current indent level.
+     *
+     * @return the indented string
+     */
+    private String indentString() {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < indent; ++i) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
 }
-
-/*end*/
