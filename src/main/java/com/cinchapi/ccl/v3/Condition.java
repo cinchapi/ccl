b921eb0ad4d9b808da0c263220426fb7acc79caf
javierlores <javier@cinchapi.com>
2019-09-06 09:20:28 -0400

    Removed logic criteria from Condition

--- src/main/java/com/cinchapi/ccl/v3/Condition.java
+++ src/main/java/com/cinchapi/ccl/v3/Condition.java
@@ -15,30 +15,14 @@
  */
 package com.cinchapi.ccl.v3;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Queue;
-import java.util.function.Function;
-
-import com.cinchapi.ccl.JavaCCParser;
-import com.cinchapi.ccl.Parser;
 import com.cinchapi.ccl.Parsing;
-import com.cinchapi.ccl.PropagatedSyntaxException;
-import com.cinchapi.ccl.SyntaxException;
 import com.cinchapi.ccl.grammar.PostfixNotationSymbol;
 import com.cinchapi.ccl.grammar.Symbol;
-import com.cinchapi.ccl.type.Operator;
-import com.cinchapi.ccl.v2.generated.CriteriaGrammar;
-import com.cinchapi.ccl.v2.generated.CriteriaGrammarPostfixVisitor;
-import com.cinchapi.ccl.v2.generated.ParseException;
-import com.cinchapi.ccl.v2.generated.SimpleNode;
-import com.cinchapi.common.base.CheckedExceptions;
-import com.cinchapi.common.function.TriFunction;
-import com.google.common.collect.Lists;
 
-import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+
+import java.util.List;
+import java.util.Queue;
 
 /**
  * A {@link Condition} is an object that is used to encapsulate the semantics of
@@ -56,86 +40,22 @@ import javax.annotation.Nullable;
  *
  * @author Jeff Nelson
  */
+/**
+ * A {@link com.cinchapi.ccl.grammar.Symbol} is a terminal or non-terminal component in a grammar.
+ *
+ * @author Jeff Nelson
+ */
+@Immutable
 public interface Condition extends Symbol {
 
     /**
-     * Return a {@link Condition} object that expresses the same as the
-     * {@code ccl} statement.
+     * Converts symbol list to Post-fix queue
      *
-     * @param ccl the CCL statement to parse
-     * @return an equivalanet {@link Condition} object
+     * @param symbolList
+     * @return
      */
-    public static Queue<PostfixNotationSymbol> parse(String ccl,
-            Function<String, Object> valueTransformFunction,
-            Function<String, Operator> operatorTransformFunction) {
-        Parser parser = Parser.create(ccl, valueTransformFunction,
-                operatorTransformFunction);
-        BuiltCondition condition = new BuiltCondition();
-        try {
-            condition.symbols = Lists.newArrayList(parser.tokenize());
-            return Parsing.toPostfixNotation(condition.symbols);
-        }
-        catch (Exception e) {
-            if(e instanceof SyntaxException
-                    || e instanceof IllegalStateException
-                    || e.getCause() != null && e
-                    .getCause() instanceof ParseException) {
-                throw e;
-            }
-            else {
-                throw CheckedExceptions.throwAsRuntimeException(e);
-            }
-        }
+    public static Queue<PostfixNotationSymbol> parse(List<Symbol> symbolList) {
+        return Parsing.toPostfixNotation(symbolList);
     }
-
-
-    /**
-     * Start building a new {@link Condition}.
-     *
-     * @return the Condition builder
-     */
-    public static StartState where() {
-        return new StartState(new BuiltCondition());
-    }
-
-    /**
-     * Return this {@link Condition} with each expression (e.g. {key} {operator}
-     * {values}) pinned to the specified {@code timestamp}.
-     *
-     * <strong>NOTE:</strong> Any timestamps that are pinned to any expressions
-     * within this Condition will be replaced by the specified {@code timestamp}.
-     *
-     * @param timestamp the {@link Timestamp} to which the returned
-     *            {@link Condition} is pinned
-     *
-     * @return this {@link Condition} pinned to {@code timestamp}
-     */
-    public Condition at(Timestamp timestamp);
-
-    /**
-     * Return a CCL string that is equivalent to this object.
-     *
-     * @return an equivalent CCL string
-     */
-    public String ccl();
-
-    /**
-     * Return a CCL string that is equivalent to this object.
-     *
-     * @return an equivalent CCL string
-     * @deprecated in favor of {@link #ccl()}
-     */
-    @Deprecated
-    public default String getCclString() {
-        return ccl();
-    }
-
-    /**
-     * Return the order list of symbols that make up this {@link Condition}.
-     *
-     * @return symbols
-     */
-    public List<Symbol> symbols();
 
 }
-
