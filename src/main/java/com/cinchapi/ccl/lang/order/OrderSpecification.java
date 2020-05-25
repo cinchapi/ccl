edb4b4980b6261bae05c814c68464438a60dda95
javierlores <javier@cinchapi.com>
2020-05-25 13:04:26 -0400

    Removed comparisiong of milliseconds from timestamps in OrderSpecification

--- src/main/java/com/cinchapi/ccl/lang/order/OrderSpecification.java
+++ src/main/java/com/cinchapi/ccl/lang/order/OrderSpecification.java
@@ -50,11 +50,28 @@ public class OrderSpecification {
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof OrderSpecification) {
+            // Remove milliseconds from Timestamp
+            // This is needed to avoid millisecond errors when comparing timestamps in
+            // @JavaCCParserLogicTest
+            int pmIndex = timestamp.toString().indexOf("PM");
+            int objPmIndex = ((OrderSpecification)obj).timestamp.toString().indexOf("PM");
+            String tempTimestamp = timestamp.toString();
+            String objTempTimestamp = ((OrderSpecification)obj).timestamp.toString();
+            if (pmIndex > 3 && objPmIndex > 3) {
+                tempTimestamp =
+                        timestamp.toString().substring(0, pmIndex - 3) + timestamp.toString().substring(pmIndex
+                                - 1);
+                objTempTimestamp =
+                        ((OrderSpecification)obj).timestamp.toString().
+                                substring(0, pmIndex - 3)
+                                + ((OrderSpecification)obj).timestamp.toString()
+                                .substring(pmIndex - 1);
+            }
+
             if (!key.equals(((OrderSpecification)obj).key)) {
                 return false;
             }
-            else if (!timestamp.toString().equals(((OrderSpecification)obj)
-                    .timestamp.toString())) {
+            else if (!tempTimestamp.equals(objTempTimestamp)) {
                 return false;
             }
             else if (!direction.equals(((OrderSpecification)obj).direction)) {
