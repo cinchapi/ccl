687d31eb66c1c1317c027e39586487ec2d618e7d
javierlores <javier@cinchapi.com>
2020-06-16 20:28:45 -0400

    Added missing period_separated_string

--- src/main/java/com/cinchapi/ccl/generated/GrammarTokenManager.java
+++ src/main/java/com/cinchapi/ccl/generated/GrammarTokenManager.java
@@ -31,7 +31,6 @@ import java.util.NoSuchElementException;
 import java.util.function.Function;
 
 /** Token Manager. */
-@SuppressWarnings("unused")
 public class GrammarTokenManager implements GrammarConstants {
 
   /** Debug output. */
