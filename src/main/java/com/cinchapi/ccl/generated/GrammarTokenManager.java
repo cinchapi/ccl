6b6be4bb786a40dc8ab09e24ffeda9a4f490d978
Jeff Nelson <jeff@cinchapi.com>
2021-06-30 10:16:11 -0400

    random cleanup

--- src/main/java/com/cinchapi/ccl/generated/GrammarTokenManager.java
+++ src/main/java/com/cinchapi/ccl/generated/GrammarTokenManager.java
@@ -33,7 +33,6 @@ import java.util.NoSuchElementException;
 import java.util.function.Function;
 
 /** Token Manager. */
-@SuppressWarnings("unused")
 public class GrammarTokenManager implements GrammarConstants {
 
   /** Debug output. */
